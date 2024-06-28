809f1a8fd7c048d78aa4dacee1e390ad0eef7e4d
ModistAndrew <zhujinxuan2005@163.com>
2024-06-28 16:43:00 +0800

    Image 12 finished

--- raytracer/src/color.rs
+++ raytracer/src/color.rs
@@ -1,5 +1,6 @@
-use crate::interval::Interval;
 use image::Rgb;
+
+use crate::interval::Interval;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Color {
@@ -29,13 +30,33 @@ impl Color {
     pub fn darken(&self, factor: f64) -> Self {
         Self::new(self.r * factor, self.g * factor, self.b * factor)
     }
+
+    fn linear_to_gamma(linear_component: f64) -> f64 {
+        if linear_component > 0.0 {
+            linear_component.sqrt()
+        } else {
+            0.0
+        }
+    }
+
+    fn r_gamma(&self) -> f64 {
+        Self::linear_to_gamma(self.r)
+    }
+
+    fn g_gamma(&self) -> f64 {
+        Self::linear_to_gamma(self.g)
+    }
+
+    fn b_gamma(&self) -> f64 {
+        Self::linear_to_gamma(self.b)
+    }
 }
 
 impl From<Color> for Rgb<u8> {
     fn from(color: Color) -> Self {
-        let r_byte = (Interval::UNIT.clamp(color.r) * 256.0) as u8;
-        let g_byte = (Interval::UNIT.clamp(color.g) * 256.0) as u8;
-        let b_byte = (Interval::UNIT.clamp(color.b) * 256.0) as u8;
+        let r_byte = (Interval::UNIT.clamp(color.r_gamma()) * 256.0) as u8;
+        let g_byte = (Interval::UNIT.clamp(color.g_gamma()) * 256.0) as u8;
+        let b_byte = (Interval::UNIT.clamp(color.b_gamma()) * 256.0) as u8;
         Rgb([r_byte, g_byte, b_byte])
     }
 }
