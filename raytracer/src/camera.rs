e8da71fd471b733c8828ec8df29102f26d68e8bb
ModistAndrew <zhujinxuan2005@163.com>
2024-07-01 16:33:10 +0800

    camera init

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -4,6 +4,23 @@ use rand::Rng;
 use crate::ray::Ray;
 use crate::vec3d::Vec3d;
 
+pub struct PerspectiveParam {
+    pub look_from: Vec3d,
+    pub look_at: Vec3d,
+    pub view_up: Vec3d,
+}
+
+pub struct LensParam {
+    pub fov: f64,
+    pub filter: Color,
+}
+
+pub struct ImageParam {
+    pub image_width: u32,
+    pub image_height: u32,
+    pub sample_per_pixel: u32,
+}
+
 pub struct Camera {
     origin: Vec3d,
     viewport_upper_left: Vec3d,
@@ -17,28 +34,35 @@ pub struct Camera {
 
 impl Camera {
     pub fn new(
-        origin: Vec3d,
-        focal_length: f64,
-        fov: f64,
-        resolution_width: u32,
-        resolution_height: u32,
-        sample_per_pixel: u32,
+        perspective_param: PerspectiveParam,
+        lens_param: LensParam,
+        canvas_param: ImageParam,
     ) -> Self {
-        let viewport_height = 2.0 * (fov.to_radians() / 2.0).tan() * focal_length;
-        let viewport_width = viewport_height * resolution_width as f64 / resolution_height as f64;
-        let viewport_u = Vec3d::new(viewport_width, 0.0, 0.0);
-        let viewport_v = Vec3d::new(0.0, -viewport_height, 0.0);
+        let direction = perspective_param.look_from - perspective_param.look_at;
+        let focal_length = direction.length();
+        let w = direction.normalize();
+        let u = (perspective_param.view_up * w).normalize();
+        let v = w * u;
+
+        let viewport_height = 2.0 * (lens_param.fov.to_radians() / 2.0).tan() * focal_length;
+        let viewport_width =
+            viewport_height * canvas_param.image_width as f64 / canvas_param.image_height as f64;
+
+        let pixel_width_ratio = viewport_width / canvas_param.image_width as f64;
+        let pixel_height_ratio = viewport_height / canvas_param.image_height as f64;
+        let viewport_u = viewport_width * u;
+        let viewport_v = viewport_height * -v;
         let viewport_upper_left =
-            origin - viewport_u / 2.0 - viewport_v / 2.0 - Vec3d::new(0.0, 0.0, focal_length);
+            perspective_param.look_from - focal_length * w - viewport_u / 2.0 - viewport_v / 2.0;
         Self {
-            origin,
+            origin: perspective_param.look_from,
             viewport_upper_left,
             viewport_u,
             viewport_v,
-            pixel_width_ratio: 1.0 / resolution_width as f64,
-            pixel_height_ratio: 1.0 / resolution_height as f64,
-            sample_per_pixel,
-            color: Color::WHITE,
+            pixel_width_ratio,
+            pixel_height_ratio,
+            sample_per_pixel: canvas_param.sample_per_pixel,
+            color: lens_param.filter,
         }
     }
 
--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,50 +1,65 @@
-use raytracer::camera::Camera;
+use raytracer::camera::{Camera, ImageParam, LensParam, PerspectiveParam};
 use raytracer::color::Color;
 use raytracer::hittable::{HittableList, Object};
-use raytracer::material::Lambertian;
+use raytracer::material::{Dielectric, Lambertian, Metal};
 use raytracer::raytracer::RayTracer;
 use raytracer::shape::Sphere;
 use raytracer::vec3d::Vec3d;
 
 fn main() {
+    let look_from = Vec3d::new(-2.0, 2.0, 1.0);
+    let look_at = Vec3d::new(0.0, 0.0, -1.0);
+    let view_up = Vec3d::new(0.0, 1.0, 0.0);
+
+    let fov = 20.0;
+    let filter = Color::WHITE;
+
     let aspect_ratio = 16.0 / 9.0;
-    let resolution_width = 400;
-
-    let focal_length = 1.0;
-    let fov = 90.0;
-    let origin = Vec3d::new(0.0, 0.0, 0.0);
-
+    let image_width = 400;
+    let mut image_height = (image_width as f64 / aspect_ratio) as u32;
+    if image_height < 1 {
+        image_height = 1;
+    }
     let sample_per_pixel = 100;
-    let max_depth = 10;
-
-    let resolution_height = (resolution_width as f64 / aspect_ratio) as u32;
-    let resolution_height = if resolution_height < 1 {
-        1
-    } else {
-        resolution_height
-    };
+    let max_depth = 50;
 
     let mut hittable_list = HittableList::default();
-    let r = (std::f64::consts::PI / 4.0).cos();
     hittable_list.add(Box::new(Object::new(
-        Box::new(Sphere::new(Vec3d::new(-r, 0.0, -1.0), r)),
-        Box::new(Lambertian::new(Color::new(0.0, 0.0, 1.0))),
+        Box::new(Sphere::new(Vec3d::new(0.0, -100.5, -1.0), 100.0)),
+        Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0))),
     )));
     hittable_list.add(Box::new(Object::new(
-        Box::new(Sphere::new(Vec3d::new(r, 0.0, -1.0), r)),
-        Box::new(Lambertian::new(Color::new(1.0, 0.0, 0.0))),
+        Box::new(Sphere::new(Vec3d::new(0.0, 0.0, -1.2), 0.5)),
+        Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5))),
+    )));
+    hittable_list.add(Box::new(Object::new(
+        Box::new(Sphere::new(Vec3d::new(-1.0, 0.0, -1.0), 0.5)),
+        Box::new(Dielectric::new(1.50)),
+    )));
+    hittable_list.add(Box::new(Object::new(
+        Box::new(Sphere::new(Vec3d::new(-1.0, 0.0, -1.0), 0.4)),
+        Box::new(Dielectric::new(1.00 / 1.50)),
+    )));
+    hittable_list.add(Box::new(Object::new(
+        Box::new(Sphere::new(Vec3d::new(1.0, 0.0, -1.0), 0.5)),
+        Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0)),
     )));
 
     let camera = Camera::new(
-        origin,
-        focal_length,
-        fov,
-        resolution_width,
-        resolution_height,
-        sample_per_pixel,
+        PerspectiveParam {
+            look_from,
+            look_at,
+            view_up,
+        },
+        LensParam { fov, filter },
+        ImageParam {
+            image_width,
+            image_height,
+            sample_per_pixel,
+        },
     );
-    let picture = raytracer::canvas::Canvas::new(resolution_width, resolution_height);
+    let picture = raytracer::canvas::Canvas::new(image_width, image_height);
     let mut raytracer = RayTracer::new(camera, picture, hittable_list, max_depth);
     raytracer.render(true);
-    raytracer.save("output/book1/image19.png");
+    raytracer.save("output/book1/image20.png");
 }
